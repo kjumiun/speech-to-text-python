dcb2b4e882e46f07ac2eaf2f1430252492c051fb
kjumiun <kjumiun@gmail.com>
2022-09-28 06:04:52 +0900

    add wer evaluate

--- wer_jvs.py
+++ wer_jvs.py
@@ -2,19 +2,23 @@ import os
 import glob
 import wer
 
-jvs = ""
+jvs = r""
 
 replace_word = str.maketrans("", "", "、。 \u3000")
 
 corpus = []
 for speaker in glob.glob(os.path.join(jvs, '*', 'parallel100' + os.sep)):
     with open(os.path.join(speaker, 'transcripts_utf8.txt'), 'r', encoding='utf8') as f:
-        transcripts = f.read().split()
-    with open(os.path.join(speaker, 'transcripts_hypothesis.txt'), 'r', encoding='utf8') as f:
-        hypothesis = f.read().split()
+        transcripts = f.read().splitlines()
+    with open(os.path.join(speaker, 'transcripts_hypothesis_whisper.txt'), 'r', encoding='utf8') as f:
+        hypothesis = f.read().splitlines()
     corpus.append([speaker, transcripts, hypothesis])
 
+word_error_total=0
+words_total=0
+
 for speaker, transcripts, hypothesis in corpus:
+    print(f'speaker: {speaker}')
     for i in range(len(transcripts)):
         r_a, r_t = transcripts[i].split(':')
         for j in range(len(hypothesis)):
@@ -23,6 +27,15 @@ for speaker, transcripts, hypothesis in corpus:
                 break
         else:
             continue
+
+        morpheme_r = wer.separateWords(r_t.translate(replace_word))
+        morpheme_h = wer.separateWords(h_t.translate(replace_word))
+        word_error, words = wer.wer(morpheme_r, morpheme_h)
+        word_error_total += word_error
+        words_total += words
+
+print("word_error_total/words_total")
+print(word_error_total,"/",words_total,"=",word_error_total/words_total)
         
 
 
